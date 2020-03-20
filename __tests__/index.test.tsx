7fff3a702955de47b4d66ac4d18cef8d8973d6dc
Vivek Pillai <vpillai@ctisinc.com>
2020-03-20 15:10:46 -0400

    with Mock router...

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
+
 import Index from '../pages/index';
+import withMockRouter from '../__mocks__/utils';
 
 describe('index page', () => {
   it('should have Layout component', () => {
-    const subject = mount(<Index />);
+    const component = mount(withMockRouter(<Index />));
 
-    expect(subject.find('Layout')).toHaveLength(1);
+    // eslint-disable-next-line
+    console.log(component);
+
+    expect(component.find('Layout')).toHaveLength(1);
   });
 });
